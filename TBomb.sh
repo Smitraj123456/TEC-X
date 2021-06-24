2c24d7d1f1be57e6a55c9c16088ef12c256ba3d3
Smitraj123456 <73018278+Smitraj123456@users.noreply.github.com>
2021-06-24 15:24:09 +0530

    Update TBomb.sh

--- TBomb.sh
+++ TBomb.sh
@@ -29,19 +29,19 @@ banner() {
     if ! [ -x "$(command -v figlet)" ]; then
         echo 'Introducing TBomb'
     else
-        figlet TBomb
+        figlet TEC-X
     fi
     if ! [ -x "$(command -v toilet)" ]; then
-        echo -e "\e[4;34m This Bomber Was Created By \e[1;32mSpeedX \e[0m"
+        echo -e "\e[4;34m This Bomber Was Created By \e[1;32mSmitraj \e[0m"
     else
         echo -e "\e[1;34mCreated By \e[1;34m"
-        toilet -f mono12 -F border SpeedX
+        toilet -f mono12 -F border RAJ
     fi
     echo -e "\e[1;34m For Any Queries Join Me!!!\e[0m"
-    echo -e "\e[1;32m           Telegram: https://t.me/TBombChat \e[0m"
-    echo -e "\e[4;32m   YouTube: https://www.youtube.com/c/SpeedXTech \e[0m"
+    echo -e "\e[1;32m           WEBSITE : techsmit12.blogspot.com \e[0m"
+    echo -e "\e[4;32m   YouTube: https://youtube.com/channel/UChwd0IfIihSZdigXLeakAwA \e[0m"
     echo " "
-    echo "NOTE: Kindly move to the PIP version Of TBomb for more stability."
+    echo "NOTE: Kindly move to the PIP version Of TEC-X for more stability."
     echo " "
 }
 
@@ -98,7 +98,7 @@ else
     echo .
     echo .
     install_deps
-    echo This Script Was Made By SpeedX > .update
+    echo This Script Was Made By SMITRAJ > .update
     echo 'Requirements Installed....'
     pause
 fi
